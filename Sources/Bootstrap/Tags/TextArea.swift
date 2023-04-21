9e65424a306c71247b7c4c988d85b5051d3a41ad
Paul <paultoffoloni@gmail.com>
2023-04-21 13:14:23 +0200

    Read TextArea value from tag body

--- Sources/Bootstrap/Tags/TextArea.swift
+++ Sources/Bootstrap/Tags/TextArea.swift
@@ -1,10 +1,13 @@
 import Leaf
 
 public struct TextAreaTag: UnsafeUnescapedLeafTag {
+    public init() { }
     public func render(_ ctx: LeafContext) throws -> LeafData {
 //        try ctx.requireNoBody()
         
-        let (classes, attributes, value) = try parseParameters(ctx)
+        let (classes, attributes) = try parseParameters(ctx)
+        
+        let body = try ctx.getRawTagBody()
 
         var textArea = "<textarea class=\"form-control"
 
@@ -18,15 +21,14 @@ public struct TextAreaTag: UnsafeUnescapedLeafTag {
             textArea += " \(attributes)"
         }
 
-        textArea += ">\(value ?? "")</textarea>"
+        textArea += ">\(body)</textarea>"
 
         return .string(textArea)
     }
 
-    func parseParameters(_ ctx: LeafContext) throws -> (classes: String?, attributes: String?, value: String?) {
+    func parseParameters(_ ctx: LeafContext) throws -> (classes: String?, attributes: String?) {
         let classes = try ctx.parse(index: 0, type: "classes")
         let attributes = try ctx.parse(index: 1, type: "attributes")
-        let value = try ctx.parse(index: 2, type: "value")
-        return (classes, attributes, value)
+        return (classes, attributes)
     }
 }
